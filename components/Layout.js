85587825be3135a8f127ee441239040c1ee8424b
Evgeny Agapov <evgenyagap@icloud.com>
2022-02-17 14:16:51 +0300

    Init

---
+++ components/Layout.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+function Layout({ home, children }) {
+	return (
+		<div>
+			<Head>
+				<link rel="icon" href="/favicon.png" />
+				<meta name="description" content="Portfolio site" />
+				<meta name={'theme-color'} />
+			</Head>
+
+			<main>{children}</main>
+		</div>
+	);
+}
+
+export default Layout;
