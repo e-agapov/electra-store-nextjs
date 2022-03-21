9616c349e1f8a54c5569803fddfe84671b207081
Evgeny Agapov <evgenyagap@icloud.com>
2022-03-21 09:44:28 +0300

    404 page is done

--- pages/404.js
+++ pages/404.js
@@ -1,3 +1,20 @@
+import Link from 'next/link';
+import styles from '../scss/pages/404.module.scss';
+
 export default function Custom404() {
-	return <h1>404 - Page Not Found</h1>;
+	return (
+		<div className={styles.wrapper}>
+			<div className={styles.infoWrapper}>
+				<div className={styles.titlePage}>Page not found</div>
+				<div className={styles.text404}>404</div>
+				<div className={styles.moreInfoText}>
+					The page you are looking for was moved, removed or maybe
+					never existed. Please go to the main page.
+				</div>
+				<Link href={'/'}>
+					<a className={styles.goHomeBtn}>Go home</a>
+				</Link>
+			</div>
+		</div>
+	);
 }
