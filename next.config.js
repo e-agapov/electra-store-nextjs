b29920d44e7e7f16350e29771f3b2414bbf70dbe
Evgeny Agapov <evgenyagap@icloud.com>
2022-04-08 23:56:38 +0300

    Next Config exec_mode: 'cluster' edited

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 module.exports = {
 	reactStrictMode: true,
-	swcMinify: true
+	swcMinify: true,
+	apps: [
+		{
+			script: 'node_modules/next/dist/bin/next',
+			args: 'start',
+			exec_mode: 'cluster'
+		}
+	]
 };
